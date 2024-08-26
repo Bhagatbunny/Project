585f7e47fd2014549f6d768d3eee8f8115aaf05a
Bhagatbunny <bhaghath19@gmail.com>
2024-08-26 22:57:48 +0530

    Update Learnings.py

--- Learnings/Learnings.py
+++ Learnings/Learnings.py
@@ -41,21 +41,21 @@ with out using elif:
 ound rounds to nearest value"""
 
 # Name= str(input("Enter name:"))
-Name = "bunn"
-height_m= float(input("Enter height in m:"))
-weight_kg = float(input("Enter weight in kg:"))
+# Name = "bunn"
+# height_m= float(input("Enter height in m:"))
+# weight_kg = float(input("Enter weight in kg:"))
 
-# bmi =round(weight_kg/(height_m * height_m)) 
-bmi = (weight_kg/(height_m * height_m))
-print("bmi:")
-print(bmi)
-# bmi=19
-if bmi<=19:
-    print(f"{Name} is Underweight")
-elif bmi>19 and bmi<=25:
-    print(f"{Name} is normal weight")
-else:
-    if bmi>25 and bmi<=31 :
-        print(f"{Name} is over weight")
-    elif bmi>31:
-        print(f"{Name} has obesity")
+# # bmi =round(weight_kg/(height_m * height_m)) 
+# bmi = (weight_kg/(height_m * height_m))
+# print("bmi:")
+# print(bmi)
+# # bmi=19
+# if bmi<=19:
+#     print(f"{Name} is Underweight")
+# elif bmi>19 and bmi<=25:
+#     print(f"{Name} is normal weight")
+# else:
+#     if bmi>25 and bmi<=31 :
+#         print(f"{Name} is over weight")
+#     elif bmi>31:
+#         print(f"{Name} has obesity")
